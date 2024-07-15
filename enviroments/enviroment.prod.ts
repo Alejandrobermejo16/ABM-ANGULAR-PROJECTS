63cbd5011bb49f1d493d85093cf21bfb435d787f
Alejandro <alejandrobermejomendez170712@gmail.com>
2024-07-15 12:57:27 +0200

    cambios

--- enviroments/enviroment.prod.ts
+++ enviroments/enviroment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: true,
     apiExchangeUrl: 'https://v6.exchangerate-api.com/v6',
-    apiExchangeKey: 'tu_clave_de_api_aqui'
+    apiExchangeKey: '2c0cdab7cb3342bbd7632816'
   };
+  
   
