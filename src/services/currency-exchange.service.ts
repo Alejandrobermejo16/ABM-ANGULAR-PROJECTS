e83736168bb8aabeb08ea098f51f4e7b36a4a7ba
Alejandro <alejandrobermejomendez170712@gmail.com>
2024-07-15 13:59:44 +0200

    puesta en produccion

--- src/services/currency-exchange.service.ts
+++ src/services/currency-exchange.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { environment } from '../../enviroments/enviroment';
+import { environment } from '../enviroments/enviroment';
 
 @Injectable({
   providedIn: 'root'
