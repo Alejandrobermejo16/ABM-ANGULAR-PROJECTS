97ba39b0608560bd71186e451a1146460d5c454c
Alejandro <alejandrobermejomendez170712@gmail.com>
2024-08-28 13:14:13 +0200

    arreglado bug de scroll infinito por menu de navegacion

--- src/components/resume-designer/resume-designer.component.ts
+++ src/components/resume-designer/resume-designer.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
 
 
 @Component({
   selector: 'app-resume-designer',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, CarouselModule, MatButtonModule, MatMenuModule],
   templateUrl: './resume-designer.component.html',
   styleUrl: './resume-designer.component.css'
 })
