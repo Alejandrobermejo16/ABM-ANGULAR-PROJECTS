80e170a62002c4a06099fdce27acbc5ae4fe05f0
Alejandro <alejandrobermejomendez170712@gmail.com>
2024-08-29 10:46:27 +0200

    añadidos estilos de boostrap, y reconstruccion de componente home

--- src/components/resume-designer/resume-designer.component.ts
+++ src/components/resume-designer/resume-designer.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
 import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { register as registerSwiperElements} from 'swiper/element/bundle';
 
 
 @Component({
@@ -11,21 +12,24 @@ import { CarouselModule } from 'ngx-bootstrap/carousel';
   standalone: true,
   imports: [CommonModule, FormsModule, CarouselModule, MatButtonModule, MatMenuModule],
   templateUrl: './resume-designer.component.html',
-  styleUrl: './resume-designer.component.css'
+  styleUrl: './resume-designer.component.css',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
+
 export class ResumeDesignerComponent implements OnInit  {
-  
   prop1: string = '';
   prop2: boolean = true;
+  slides = [
+    { image: '/CambioMoneda.jpg', caption: 'First slide' },
+    { image: '/CambioMoneda.jpg', caption: 'Second slide' },
+    { image: '/CambioMoneda.jpg', caption: 'Third slide' }
+  ];
 
   ngOnInit(): void {
     throw new Error('Method not implemented.');
   }
 
-  constructor() {}
-  
-
-
-
-
+  constructor() {
+    registerSwiperElements();
+  }
 }
