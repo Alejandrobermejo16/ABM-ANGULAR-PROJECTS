dcf7cc41a5b7d4d31a14a3e9f8b64925bbd0757f
Alejandro <alejandrobermejomendez170712@gmail.com>
2025-01-27 14:13:20 +0100

    feat: create const with images for function of change page in angular info

---
+++ src/components/info-angular/constants.ts
@@ -0,0 +1,8 @@
+export const IMAGES_PATH = [
+    {"title":"¿QUÉ ES ANGULAR?", "path":"angularInfo/0-Angular.png"},
+    {"title":"DEFINICIÓN DE TIPOS DE VARIABLES", "path":"angularInfo/1-Definicion_tipo_variable.png"},
+    {"title":"INICIALIZACIÓN DE VARIABLES EN EL CONSTRUCTOR", "path":"angularInfo/2-ExplicacionConstructor.png"},
+    {"title":"ASIGNACIÓN DE VALORES EN EL CICLO DE VIDA OnInit", "path":"angularInfo/3-Asignaciondevaloresenvariablesoninit.png"},
+
+    
+];
--- src/components/info-angular/info-angular.component.ts
+++ src/components/info-angular/info-angular.component.ts
@@ -1,30 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { IMAGES_PATH } from './constants';
+
 @Component({
   selector: 'app-info-angular-component',
   standalone: true,
   imports: [CommonModule, FormsModule],
   templateUrl: './info-angular.component.html',
   styleUrls: ['./info-angular.component.css'],
-  
 })
 export class InfoAngular implements OnInit {
-  valueName: string | undefined;
-  holder: string | undefined;
-  disabled: boolean = false;
-  activeButton: boolean = false;
+  ArrayImages: Array<{ title: string, path: string }> = [];
+  image: string = '';
+  dinamicTitle: string | undefined;
+  page!: number;
+  totalPages!: number;
 
   ngOnInit() {
-    this.holder = 'escribe en el siguiente input'
+    this.ArrayImages = IMAGES_PATH;
+    if (this.ArrayImages.length > 0) {
+      this.image = this.ArrayImages[0].path;
+      this.dinamicTitle = this.ArrayImages[0].title;
+      this.page = 0;
+      this. totalPages = this.ArrayImages.length -1;
+    }
   }
 
-  changeName(name: any) {
-    this.disabled = true;
-    this.activeButton = true;
-  }
-  activeButtonEvent(){
-    this.activeButton = false;
-    this.disabled = false;
+  changeImage(direction: number) {
+    const currentIndex = this.ArrayImages.findIndex((img) => img.path === this.image);
+    let newIndex = (currentIndex + direction + this.ArrayImages.length) % this.ArrayImages.length;
+    this.page = newIndex;
+    this.image = this.ArrayImages[newIndex].path;
+    let newTitle = this.ArrayImages[newIndex].title;
+    this.dinamicTitle = newTitle;
   }
 }
