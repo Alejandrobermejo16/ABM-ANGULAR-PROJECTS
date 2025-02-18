b11452513c2c8666632c32881ce4da20c292ec0b
Alejandro <alejandrobermejomendez170712@gmail.com>
2025-02-18 15:31:13 +0100

    feat: add upload and image

--- src/components/resume-designer/cv-type/cv-type.component.ts
+++ src/components/resume-designer/cv-type/cv-type.component.ts
@@ -1,29 +1,42 @@
 import { Component, OnInit, CUSTOM_ELEMENTS_SCHEMA, Input } from '@angular/core';
-import { CommonModule, } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { ProgressbarModule } from 'ngx-bootstrap/progressbar';
 import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-cv-type',
   standalone: true,
-  imports: [CommonModule,ProgressbarModule,FormsModule],
+  imports: [CommonModule, ProgressbarModule, FormsModule],
   templateUrl: './cv-type.html',
-  styleUrl: './cv-type.css',
+  styleUrls: ['./cv-type.css'],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-
-export class CvTypeComponent implements OnInit  {
+export class CvTypeComponent implements OnInit {
   @Input() name: string = '';
   @Input() telephone: string = '';
   @Input() email: string = '';
   @Input() linkedin: string = '';
   @Input() fields: object = {};
-  editField(){
+  imgUpload: string = '';
+  
+  editField() {
     console.log('editField');
   }
 
+  onFileSelected(event: any) {
+    const file: File = event.target.files[0]; 
+    if (file) {
+      const reader = new FileReader(); 
+      reader.readAsDataURL(file);
+
+      reader.onload = () => {
+        this.imgUpload = reader.result as string;
+      };
+    }
+  }
+
   ngOnInit(): void {
+    // Puedes inicializar valores aquí si es necesario
   }
-  
-  constructor() {
-  }
+
+  constructor() {}
 }
